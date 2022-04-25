d43958c29e929ded7f49c998690c3c90643284cb
Swarup Das <swarup.das@airpay.com>
2022-04-25 18:43:31 +0530

    todo frontend using new ui lib and new built tool

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+   <a-button type="primary">Primary Button</a-button>
+</template>
+
+<style>
+
+</style>
